11410ab579975cb2e0981c8ea6d10136ca95c425
benbenaod <132361502+benbenaod@users.noreply.github.com>
2026-01-06 02:32:20 +0800

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -24,6 +24,5 @@ path("personal/remove/<int:course_id>/", views.remove_personal_course, name="rem
 
 path("teacher-info/", views.teacher_info, name="teacher_info"),
 path("debug_db/", views.debug_db, name="debug_db"),
-path("demo-logout/", views.demo_logout_view, name="demo_logout"),
 path("demo-login/", views.demo_login_view, name="demo_login_view"),
 ]
