553e87cb52825e61e9e41caf7db93ea64c9402f3
benbenaod <132361502+benbenaod@users.noreply.github.com>
2026-01-10 04:14:32 +0800

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -21,8 +21,7 @@ path("delete-course/<int:course_id>/", views.delete_course, name="delete_course"
 # ✅ 個人課表（session 版）
 path("personal/add/<int:course_id>/", views.add_personal_course, name="add_personal_course"),
 path("personal/remove/<int:course_id>/", views.remove_personal_course, name="remove_personal_course"),
-path("demo-logout/", views.demo_login_view, name="demo_logout"),
+
 path("teacher-info/", views.teacher_info, name="teacher_info"),
-path("debug_db/", views.debug_db, name="debug_db"),
-path("demo-login/", views.demo_login_view, name="demo_login_view"),
+
 ]
